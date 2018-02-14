b4860a90bcc05f61a1a89aa99822b78034816c7b
Max Vorobyev <gtvolk31@gmail.com>
2018-02-14 04:24:01 +0300

    Added docker deployment support

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ var Server = {
         me.server = http.createServer(app);
         me.socket = io.listen(me.server);
         me.db = mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "",
-            database : "extchatdb"
+            host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT || 3306,
+            user: process.env.DB_USER || 'root',
+            password: process.env.DB_PASSWORD || '',
+            database : process.env.DB_NAME || 'extchatdb'
         });
         me.db.connect(function(err) {
             if (err) {
