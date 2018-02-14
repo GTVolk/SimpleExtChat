e5874a573e37bb40846299d6c124703ee9d91c0b
Max Vorobyev <gtvolk31@gmail.com>
2018-02-14 13:12:11 +0300

    Added codestyle, logging (winston), some gitignore, refreshed server

--- server.js
+++ server.js
@@ -1,151 +1,225 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var io = require('socket.io');
-var port = process.env.PORT || 3000;
-var mysql = require('mysql');
+const express = require('express');
+const http = require('http');
+const io = require('socket.io');
+const winston = require('winston');
+const moment = require('moment');
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'extchatdb',
+  },
+  acquireConnectionTimeout: 60000,
+});
 
-var Server = {
-    init : function () {
-        var me = this;
-        me.userCount = 0;
-        me.server = http.createServer(app);
-        me.socket = io.listen(me.server);
-        me.db = mysql.createConnection({
-            host: process.env.DB_HOST || 'localhost',
-            port: process.env.DB_PORT || 3306,
-            user: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || '',
-            database : process.env.DB_NAME || 'extchatdb'
+const port = process.env.PORT || 3000;
+const app = express();
+const server = http.Server(app);
+const env = process.env.NODE_ENV !== 'production' ? 'development' : 'production';
+const logLevel = process.env.LOG_LEVEL || (env === 'development' ? 'debug' : 'info');
+const logger = winston.createLogger({
+  level: logLevel,
+  format: winston.format.json(),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log`
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({
+      filename: 'error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: 'combined.log',
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+server.listen(port, () => {
+  logger.info(`Server listening on port ${port}`);
+});
+const socketIO = io.listen(server);
+
+if (env === 'production') {
+  app.use(express.static(`build/${env}/ChatApp`));
+} else {
+  app.use(express.static(`build/${env}/ChatApp`));
+  app.use(express.static('ChatApp'));
+  app.use(express.static('ext'));
+}
+
+knex.select(knex.raw('1+1 AS result'))
+  .then(() => {
+    logger.info('MySQL connection established');
+  })
+  .catch((err) => {
+    logger.error(`Error on connecting mysql database: ${err.stack}`);
+  });
+
+let clientsCount = 0;
+
+socketIO.on('connection', (socket) => {
+  let user;
+
+  logger.info('Client connected');
+  logger.debug(`Client with ID=${socket.client.id} connected from ADDRESS=${socket.handshake.address} on TIME=${socket.handshake.time}`);
+
+  socket.on('login', (data) => {
+    if (user) return;
+
+    logger.info('Client loggged in');
+    logger.debug(`USER=${JSON.encode(user)} with DATA=${JSON.encode(data)}`);
+
+    clientsCount += 1;
+
+    user = {
+      username: data.username,
+    };
+
+    const getLostMessages = () =>
+      knex.select([
+        'username',
+        'post_date',
+        'text',
+      ])
+        .from('messages')
+        .join('users', 'user_id', 'users.id')
+        .where('post_date', '>', user.was_online)
+        .orderBy('post_date')
+        .map(message => ({
+          post_date: moment(message.post_date),
+          username: message.username,
+          message: message.text.toString(),
+        }))
+        .then((messages) => {
+          socket.emit('user_logged_in', {
+            username: user.username,
+            online: clientsCount,
+            messages,
+          });
+          socketIO.broadcast.emit('user_logged_in', {
+            username: user.username,
+            online: clientsCount,
+          });
         });
-        me.db.connect(function(err) {
-            if (err) {
-                console.error("Error on connecting mysql database: " + err.stack);
-                return;
-            }
-            console.log("Mysql connection established as id: " + me.db.threadId);
+
+    const onUserInsert = (id) => {
+      if (!user.id) {
+        user.id = id;
+      }
+
+      return getLostMessages();
+    };
+
+    knex.select([
+      'id',
+      'was_online',
+    ])
+      .from('users')
+      .where('username', data.username)
+      .limit(1)
+      .first()
+      .then((dbUser) => {
+        if (!dbUser) {
+          user.was_online = moment();
+          return knex('users')
+            .insert({
+              username: data.username,
+              was_online: user.was_online,
+            })
+            .returning('id')
+            .then(id => onUserInsert(id));
+        }
+
+        user.id = dbUser.id;
+        user.was_online = dbUser.was_online;
+        return knex('users')
+          .update({
+            was_online: moment(),
+          })
+          .where('id', dbUser.id)
+          .returning('id')
+          .then(id => onUserInsert(id));
+      });
+  });
+
+  socket.on('starttype', () => {
+    if (!user) return;
+
+    logger.info('Client typing message');
+    logger.debug(`Typing USER=${JSON.encode(user)}`);
+
+    socketIO.emit('user_typing_start', {
+      username: user.username,
+    });
+  });
+
+  socket.on('endtype', () => {
+    if (!user) return;
+
+    logger.info('Client ended typing');
+    logger.debug(`Ended typing USER=${JSON.encode(user)}`);
+
+    socketIO.emit('user_typing_end', {
+      username: user.username,
+    });
+  });
+
+  socket.on('chat_message', (message) => {
+    if (!user) return;
+
+    logger.info('Client sended message');
+    logger.debug(`USER=${JSON.encode(user)} sended MESSAGE=${JSON.encode(message)}`);
+
+    const postDate = moment();
+
+    knex('messages')
+      .insert({
+        user_id: user.id,
+        post_date: postDate,
+        message,
+      })
+      .then(() => {
+        socketIO.emit('new_message', {
+          username: user.username,
+          message,
+          post_date: postDate,
         });
-        me.socket.on("connection", function(client) {
-            var user;
+      });
+  });
 
-            console.log("Client " + client.client.id + " connected from " + client.handshake.address + " on " + client.handshake.time);
+  const logoutFn = event => () => {
+    if (!user) return;
 
-            client.on("login", function(data) {
-                if (user) return;
-                console.log("Client login", user, data);
-                ++me.userCount;
-                user = {username : data.username};
-                var messages = [];
+    logger.info('Client logged out');
+    logger.debug(`USER=${JSON.encode(user)}`);
 
-                var onSelectOnline = function (err, results) {
-                    if (err) throw err;
-                    if (!results || !results.length) {
-                        user.was_online = new Date(0);
-                        me.db.query("INSERT INTO users(username, was_online) VALUES(?, ?)", [data.username, new Date()], onUserInsert);
-                    } else {
-                        user.id = results[0].id;
-                        user.was_online = results[0].was_online;
-                        me.db.query("UPDATE users SET was_online = ? WHERE username = ?", [new Date(), data.username], onUserInsert);
-                    }
-                };
+    clientsCount -= 1;
 
-                var onUserInsert = function(err, results) {
-                    if (err) throw err;
-                    if (!user.id) {
-                        user.id = results.insertId;
-                    }
-                    getLostMessages();
-                };
-
-                var getLostMessages = function() {
-                    me.db.query("SELECT u.username, m.post_date, m.text FROM messages m JOIN users u ON (m.user_id = u.id) WHERE m.post_date > ? ORDER BY m.post_date", [user.was_online], onGetMessages);
-                };
-
-                var onGetMessages = function(err, results) {
-                    if (err) throw err;
-                    messages = [];
-                    for(var i = 0; i < results.length; i++) {
-                        messages.push({
-                            post_date : new Date(results[i].post_date),
-                            username : results[i].username,
-                            message : results[i].text.toString()
-                        })
-                    }
-                    sendToClient();
-                };
-
-                var sendToClient = function() {
-                    client.emit('user_logged_in', {
-                        username: user.username,
-                        online: me.userCount,
-                        messages : messages
-                    });
-                    client.broadcast.emit('user_logged_in', {
-                        username: user.username,
-                        online: me.userCount
-                    });
-                };
-                me.db.query("SELECT id, was_online FROM users WHERE username = ?", [data.username], onSelectOnline);
-            });
-
-            client.on("starttype", function() {
-                if (!user) return;
-                console.log("Client typing", user);
-                client.broadcast.emit('user_typing_start', {
-                    username: user.username
-                });
-            });
-
-            client.on("endtype", function() {
-                if (!user) return;
-                console.log("Client ends typing", user);
-                client.broadcast.emit("user_typing_end", {
-                    username : user.username
-                });
-            });
-
-            client.on("chat_message", function(message) {
-                if (!user) return;
-                console.log("Client sended message", user, message);
-                var post_date = new Date();
-                me.db.query("INSERT INTO messages(user_id, post_date, text) VALUES(?,?,?)", [user.id, post_date, message], function(err, results) {
-                    if (err) throw err;
-                    client.broadcast.emit("new_message", {
-                        username : user.username,
-                        message : message,
-                        post_date : post_date
-                    });
-                });
-            });
-
-            logoutFn = function(event) {
-                return function() {
-                    if (!user) return;
-                    console.log("Client logout", user);
-                    --me.userCount;
-                    me.db.query("UPDATE users SET was_online = ? WHERE username = ?", [new Date(), user.username], function(err) {
-                        if (err) throw err;
-                        client.broadcast.emit(event, {
-                            username: user.username,
-                            online: me.userCount
-                        });
-                        user = null;
-                    });
-                }
-            };
-
-            client.on("logout", logoutFn("user_logged_out"));
-            client.on("close", logoutFn("connection_close"));
-            client.on("disconnect", logoutFn("user_disconnected"));
+    knex('users')
+      .update({
+        was_online: moment(),
+      })
+      .where('id', user.id)
+      .then(() => {
+        socketIO.emit(event, {
+          username: user.username,
+          online: clientsCount,
         });
-    },
+        user = null;
+      });
+  };
 
-    connect : function (port) {
-        var me = this;
-        me.server.listen(port);
-        console.log("Server listening at port %d", port);
-    }
-};
-
-Server.init();
-Server.connect(port);
+  socket.on('logout', logoutFn('user_logged_out'));
+  socket.on('close', logoutFn('connection_close'));
+  socket.on('disconnect', logoutFn('user_disconnected'));
+});
